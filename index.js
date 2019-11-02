3cfd3992199fc62d97d34e790db502f0f17fbde1
Robert Ferris <robert.ferris@codedx.com>
2019-11-02 11:16:39 -0400

    Change to single accessory rather than platform

--- index.js
+++ index.js
@@ -1,55 +1,30 @@
 var request = require("request");
 var poll = require("polling-to-event");
 
-var Accessory, Service, Characteristic, UUIDGen;
+var Service, Characteristic;
 
 module.exports = function(homebridge) {
-	var Service = homebridge.hap.Service;
-	var Characteristic = homebridge.hap.Characteristic;
-
-	Accessory = homebridge.platformAccessory;
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
-	UUIDGen = homebridge.hap.uuid;
 
-	homebridge.registerAccessory("homebridge-proteus-sensor", "ProteusSensor", ProteusSensor, true);
+	homebridge.registerAccessory("homebridge-proteus-sensor", "ProteusSensor", ProteusSensor);
 }
 
-function ProteusSensor(log, config, api) {
-	var platform = this;
+function ProteusSensor(log, config) {
 	this.log = log;
 	this.config = config;
-	this.api = api;
 
-	if (config.sensors) {
-		config.sensors.forEach(function(sensor) {
-			platform.sensors[sensor.name] = {
-				name: sensor.name,
-				uuid: UUIDGen.generate(sensor.name),
-				type: sensor.type,
-				invert: !!sensor.invert,
-				host: sensor.host,
-				url: sensor.url || ('http://' + sensor.host + '/status.json'),
-				interval: sensor.interval || 2000,
-			};
-		});
-	}
-
-	this.api.on('didFinishLaunching', function() {
-		platform.log('finished launching, configuring new accessories...');
-		platform.sensors.forEach(function(sensor) {
-			if (!sensor.accessory) {
-				sensor.accessory = new Accessory(sensor.name, sensor.uuid);
-				sensor.accessory.reachable = false; // we'll update at first poll
-				platform.setupSensor(sensor);
-			}
-		});
-	});
+	this.name = config.name;
+	this.type = config.type;
+	this.invert = !!config.invert;
+	this.host = config.host;
+	this.url = config.url || ('http://' + sensor.host + '/status.json');
+	this.interval = config.interval || 2000;
 };
 
-ProteusSensor.prototype.configureAccessory = function(accessory) {
+/*ProteusSensor.prototype.configureAccessory = function(accessory) {
 	var platform = this;
-	platform.log(accessory.displayName, "configure accessory");
+	this.log(accessory.displayName, "configure accessory");
 
 	// we'll mark it reachable once we poll it for the first time
 	accessory.reachable = false;
@@ -59,49 +34,45 @@ ProteusSensor.prototype.configureAccessory = function(accessory) {
 
 	sensor.accessory = accessory;
 	this.setupSensor(sensor);
-};
+};*/
 
-ProteusSensor.prototype.setupSensor = function(sensor) {
-	var platform = this;
-	platform.log(sensor.name, "setup sensor communications");
-
+ProteusSensor.prototype.identify = function(callback) {
 	// we don't have a way to identify
-	sensor.accessory.on('identify', function(paired, callback) {
-		platform.log(sensor.name, "identify requested");
-		callback();
-	});
+	this.log(this.name, "identify requested");
+	callback();
+}
 
-	sensor.infoService = sensor.accessory.getService(Service.AccessoryInformation);
-	if (!sensor.infoService) sensor.infoService = sensor.accessory.addService(Service.AccessoryInformation);
-	sensor.infoService
+ProteusSensor.prototype.getServices = function() {
+	this.log(this.name, "get services");
+
+	this.infoService = new Service.AccessoryInformation()
 		.setCharacteristic(Characteristic.Manufacturer, "Proteus")
 		.setCharacteristic(Characteristic.SerialNumber, "Unknown")
 		.setCharacteristic(Characteristic.FirmwareRevision, "Unknown");
 
 	switch (sensor.type) {
 		case 'level':
-			sensor.infoService.setCharacteristic(Characteristic.Model, "Level Sensor");
+			this.infoService.setCharacteristic(Characteristic.Model, "Level Sensor");
 
-			sensor.leakService = sensor.accessory.getService(Service.LeakSensor);
-			if (!sensor.leakService) sensor.leakService = sensor.accessory.addService(Service.LeakSensor);
-			sensor.leakDetected = sensor.leakService.getCharacteristic(Characteristic.LeakDetected);
-			sensor.statusActive = sensor.leakService.getCharacteristic(Characteristic.StatusActive);
-			sensor.statusFault = sensor.leakService.getCharacteristic(Characteristic.StatusFault);
+			this.leakService = new Service.LeakSensor();
+			this.leakDetected = this.leakService.getCharacteristic(Characteristic.LeakDetected);
+			this.statusActive = this.leakService.getCharacteristic(Characteristic.StatusActive);
+			this.statusFault = this.leakService.getCharacteristic(Characteristic.StatusFault);
 
-			platform.setupPolling(sensor);
+			this.setupPolling();
 
 			break;
 
 		default:
-			sensor.infoService.setCharacteristic(Characteristic.Model, "Unknown");
-			platform.log(sensor.name, "unknown sensor type: " + sensor.type);
+			this.infoService.setCharacteristic(Characteristic.Model, "Unknown");
+			this.log(sensor.name, "unknown sensor type: " + sensor.type);
 			break;
 	}
 };
 
-ProteusSensor.prototype.setupPolling = function(sensor) {
-	var platform = this;
-	platform.log(sensor.name, "intialize polling");
+ProteusSensor.prototype.setupPolling = function() {
+	var sensor = this;
+	this.log(sensor.name, "intialize polling");
 
 	sensor.emitter = poll(function(done) {
 		request({ method: 'GET', url: sensor.url }, function(err, resp, data) { done(err, JSON.parse(data)); });
